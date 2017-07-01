6c818018b933d4e9c43b5e44fbce82ef4f99920a
Mariana Meirelles <mmeirell@ucsd.edu>
2017-07-01 10:22:51 -0700

    Added editing functionality, but still getting error when tryint to load show.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,16 @@ end
 def show
     @post = Post.find(params[:id])    
 end
+    
+def edit
+    @post = Post.find(params[:id])    
+end
+
+def update
+    @post = Post.find(parans[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+end
 
 private 
 
